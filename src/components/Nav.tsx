d0f7a4aa79167ec1e28bc77691998e9c846a9903
Vijay <vijaynatarajan@protonmail.com>
2025-02-20 15:52:16 -0500

    nav bar completed

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom'
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/SavedCandidates">Potential Candidates</Link>
+    </nav>
   )
 };
 
